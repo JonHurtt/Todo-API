66e71097b572084b8896d302985cd04e67bb6a47
Jonathan R. Hurtt <JonHurtt@gmail.com>
2016-03-03 08:02:08 -0500

    Added DELETE todos route

--- server.js
+++ server.js
@@ -10,45 +10,34 @@ var todoNextID = 1;
 //Setting up Middleware
 app.use(bodyParser.json());//anytime a JSON request comes in, Express parses it and we can access it via bodyParser
 
+//Get Root
 app.get('/', function(request, response){
+	console.log("GET <root>");
 	response.send('To Do API Root');
 });
 
 //GET /todos
 app.get('/todos', function(request, response){
+	console.log("GET /todos");
 	response.json(todos);	
 });
 
 
 //GET /todos/:id
 app.get('/todos/:id', function(request, response){
+	console.log("GET /todos/:id");
 	//response.send('Asking for todo with id of ' + request.params.id);
 	
 	var todoID = parseInt(request.params.id, 10);//all request are a string, you have to convert to a number
 	
-	/*
-	var matchedTodo;
-	todos.forEach(function(todo){
-		console.log('Looking for ID: ' + todoID)
-		if(todoID === todo.id){
-			console.log('Found ID: ' + todoID)
-			matchedTodo = todo;
-		}
-	});
-	*/	
-	
-	//Refactor using Underscore
+		//Refactor using Underscore
 	var matchedTodo = _.findWhere(todos, {id: todoID});
 	
 	if(typeof matchedTodo === 'undefined'){
-		response.status(404).send();
+		response.status(404).json({"error" : "No ToDo found with id of " + todoID + ". Cannot GET"});
 	}else{
 		response.json(matchedTodo);
-	}
-	
-	
-	
-	
+	}	
 });
 
 
@@ -58,7 +47,7 @@ app.post('/todos', function(request, response){
 	//Save Body and reomve unwanted keys
 	var body = _.pick(request.body, 'id', 'description', 'completed')
 	
-	//Validation for String or Boolean or Empty String
+	//Validation for String or Boolean or Empty String and sanitize string
 	if(!_.isString(body.description) ||  !_.isBoolean(body.completed) || (body.description.trim().length === 0)){
 		response.status(404).send("Error 404");
 	}else{
@@ -70,6 +59,24 @@ app.post('/todos', function(request, response){
 	
 });
 
+
+//Delete /todos/:id
+app.delete('/todos/:id', function(request, response){
+	console.log("DELETE /todos/:id");
+	
+	var todoID = parseInt(request.params.id, 10);//all request are a string, you have to convert to a number
+		
+	//Refactor using Underscore
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+	
+	if(typeof matchedTodo === 'undefined'){
+		response.status(404).json({"error:" : "No ToDo found with id of " + todoID + ". Cannot DELETE"});
+	}else{
+		todos = _.without(todos, matchedTodo);
+		response.json(matchedTodo);
+	}
+		
+});
 
 
 app.listen(PORT, function(){
