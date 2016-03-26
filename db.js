f682c5ce7bef497967937d04fe0cefd91e300358
Jonathan R. Hurtt <JonHurtt@gmail.com>
2016-03-26 14:51:57 -0400

    Add Postgres Support for Produciton/Heroku

--- db.js
+++ db.js
@@ -1,10 +1,25 @@
 //initialize the Sequelize
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production'){
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		'dialect' : 'postgres'
+	});
+}else{
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect' : 'sqlite',
+		'storage' : __dirname + '/data/dev-todo-api.sqlite'
+	});	
+}
+
+/*
 var sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect' : 'sqlite',
 	'storage' : __dirname + '/data/dev-todo-api.sqlite'
 });
-
+*/
 
 //Create Database Object
 var db = {};
